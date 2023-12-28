7c437235fc545ba1d21825be070f5dbdafc9d7f5
aibayanyu20 <aibayanyu@qq.com>
2023-12-28 20:28:26 +0800

    ci: eslint format

--- playground/app.vue
+++ playground/app.vue
@@ -54,19 +54,26 @@
           notification info
         </a-button>
       </a-space>
-      <a-flex gap="middle" vertical>
-    <a-radio-group v-model:value="value">
-      <a-radio value="horizontal">horizontal</a-radio>
-      <a-radio value="vertical">vertical</a-radio>
-    </a-radio-group>
-    <a-flex :vertical="value === 'vertical'">
-      <div
-        v-for="(item, index) in new Array(4)"
-        :key="item"
-        :style="{ ...baseStyle, background: `${index % 2 ? '#1677ff' : '#1677ffbf'}` }"
-      />
-    </a-flex>
-  </a-flex>
+      <a-flex
+        gap="middle"
+        vertical
+      >
+        <a-radio-group v-model:value="value">
+          <a-radio value="horizontal">
+            horizontal
+          </a-radio>
+          <a-radio value="vertical">
+            vertical
+          </a-radio>
+        </a-radio-group>
+        <a-flex :vertical="value === 'vertical'">
+          <div
+            v-for="(item, index) in new Array(4)"
+            :key="item"
+            :style="{ ...baseStyle, background: `${index % 2 ? '#1677ff' : '#1677ffbf'}` }"
+          />
+        </a-flex>
+      </a-flex>
     </div>
   </a-config-provider>
 </template>
