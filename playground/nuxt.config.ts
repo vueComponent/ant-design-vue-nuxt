5112ceea6a39252654bb28186a0ee4d5c9dc9907
aibayanyu20 <aibayanyu@qq.com>
2024-09-27 21:13:45 +0800

    chore: update deps

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,9 +1,13 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
+
   antd: {
     extractStyle: true
   },
+
   imports:{
     autoImport: true
-  }
+  },
+
+  compatibilityDate: '2024-09-27'
 })
